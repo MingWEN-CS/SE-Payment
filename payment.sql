dd213d74a7b5052bd0d8871eee29f764a37439bb
justinwm <justinwm@163.com>
2013-06-12 09:33:08 +0800

    add group1's database

--- payment.sql
+++ payment.sql
@@ -6,9 +6,103 @@ GRANT ALL PRIVILEGES ON payment.* TO 'laolao'@'localhost';
 USE payment;
 DROP TABLE IF EXISTS user;
 /* group 1 */
-CREATE TABLE user(
-	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
-);
+
+--
+-- Tabellenstruktur für Tabelle `account`
+--
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `AID` char(50) NOT NULL DEFAULT '',
+  `TYPE` bit(1) DEFAULT NULL,
+  `UID` char(50) DEFAULT NULL,
+  `BALANCE` decimal(12,2) DEFAULT NULL,
+  `BANKACCOUNT` char(50) DEFAULT NULL,
+  PRIMARY KEY (`AID`),
+  KEY `UID` (`UID`,`TYPE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `buyer`
+--
+
+CREATE TABLE IF NOT EXISTS `buyer` (
+  `USERNAME` char(50) DEFAULT NULL,
+  `EMAIL` char(50) NOT NULL DEFAULT '',
+  `PASSWORDLOGIN` char(50) DEFAULT NULL,
+  `PASSWORDPAYMENT` char(50) DEFAULT NULL,
+  `BALANCE` decimal(12,2) DEFAULT NULL,
+  `VIP` bit(1) DEFAULT NULL,
+  `AUTHENTICATED` bit(1) DEFAULT NULL,
+  `CREDIT` char(50) DEFAULT NULL,
+  PRIMARY KEY (`EMAIL`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `receiveaddress`
+--
+
+CREATE TABLE IF NOT EXISTS `receiveaddress` (
+  `ADDRESSID` char(50) NOT NULL DEFAULT '',
+  `UID` char(50) DEFAULT NULL,
+  `TYPE` bit(1) DEFAULT NULL,
+  `RECEIVERNAME` char(50) DEFAULT NULL,
+  `RECEIVERPHONE` char(20) DEFAULT NULL,
+  `PROVINCE` char(50) DEFAULT NULL,
+  `CITY` char(50) DEFAULT NULL,
+  `STRICT` char(50) DEFAULT NULL,
+  `STREET` char(100) DEFAULT NULL,
+  PRIMARY KEY (`ADDRESSID`),
+  KEY `UID` (`UID`,`TYPE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `seller`
+--
+
+CREATE TABLE IF NOT EXISTS `seller` (
+  `USERNAME` char(50) DEFAULT NULL,
+  `EMAIL` char(50) NOT NULL DEFAULT '',
+  `PASSWORDLOGIN` char(50) DEFAULT NULL,
+  `BALANCE` decimal(12,2) DEFAULT NULL,
+  PRIMARY KEY (`EMAIL`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `EMAIL` char(50) NOT NULL DEFAULT '',
+  `TYPE` bit(1) NOT NULL DEFAULT b'0',
+  `USERNAME` char(50) DEFAULT NULL,
+  `PHONE` char(20) DEFAULT NULL,
+  PRIMARY KEY (`EMAIL`,`TYPE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`UID`, `TYPE`) REFERENCES `user` (`EMAIL`, `TYPE`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `receiveaddress`
+--
+ALTER TABLE `receiveaddress`
+  ADD CONSTRAINT `receiveaddress_ibfk_1` FOREIGN KEY (`UID`, `TYPE`) REFERENCES `user` (`EMAIL`, `TYPE`) ON DELETE CASCADE;
+
 
 /* group 2 */
 CREATE TABLE transaction(
