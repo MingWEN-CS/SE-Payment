e752440fdd77291b565a7b4bc4839ff153498a31
DinglongDuan <dinglong.duan@gmail.com>
2013-06-12 11:53:47 +0800

    admin login gui

--- Lib/Action/AdminAction.class.php
+++ Lib/Action/AdminAction.class.php
@@ -2,8 +2,12 @@
 // 本类由系统自动生成，仅供测试用途
 class AdminAction extends Action {
     
-    public function index(){
-        $this->display("index");
+    public function index (){
+        $this->display("login");
     }    
+
+    public function header () {
+    	$this->display();
+    }
 }
 ?>
