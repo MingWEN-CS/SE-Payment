2956dde8e6dde09ccf36e2296b8ebf8c4732412a
youngjerky <jueji.yang@gmail.com>
2013-06-13 13:38:18 +0800

    merge payment db;use payment to update

--- payment.sql
+++ payment.sql
@@ -2,21 +2,59 @@
 DROP DATABASE IF EXISTS payment;
 CREATE DATABASE payment;
 /* if failed on following CREATE USER sql due to exits laolao already just delete it and redo >.<*/
-CREATE USER 'laolao'@'localhost' IDENTIFIED BY 'laolao';
+-- CREATE USER 'laolao'@'localhost' IDENTIFIED BY 'laolao';
 GRANT ALL PRIVILEGES ON payment.* TO 'laolao'@'localhost';
 USE payment;
 /* if you are using sqlite please start here */
-DROP TABLE IF EXISTS user;
-/* group 1 */
-CREATE TABLE user(
-	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
-	/* if you are using sqlite please use following instead */
-	/* id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT */
-);
-=======
+/* key tables has referenced foreign keys*/
+-- --------------------------------------------------------
 
 --
+-- Table structure for table `User`
+--
 
+CREATE TABLE IF NOT EXISTS `User` (
+  `UID` int(10) NOT NULL AUTO_INCREMENT,
+  `USERNAME` char(20) CHARACTER SET utf8 NOT NULL,
+  `PASSWORD` char(32) CHARACTER SET utf8 NOT NULL,
+  `EMAIL` char(30) CHARACTER SET utf8 NOT NULL,
+  `TYPE` tinyint(1) NOT NULL,
+  `BALANCE` int(11) DEFAULT '0',
+  `PHONE` char(11) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`UID`),
+  UNIQUE KEY `ID` (`UID`),
+  UNIQUE KEY `USERNAME` (`USERNAME`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+DROP TABLE IF EXISTS goods;
+CREATE TABLE goods(
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	/* if you are using sqlite please use following instead */
+	/* id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT */
+	type INTEGER NOT NULL
+	/*	1: general goods
+		2: hotel room
+		3: airplane ticket
+	It is better to use get funcions in GeneralGoodsModel and other models*/
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders(
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	/* if you are using sqlite please use following instead */
+	/* id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT */
+	buyer char(50) NOT NULL,
+	seller char(50) NOT NULL,
+	totalprice numeric(15,2),
+	isdelete bit(1) NOT NULL,
+	state char(20) NOT NULL,
+	foreign key (buyer) references User(USERNAME) on delete cascade,
+	foreign key (seller) references User(USERNAME) on delete cascade
+);
+
+
+/* group 1 */
+--
 --
 CREATE TABLE IF NOT EXISTS `Buyer` (
   `UID` int(11) NOT NULL,
@@ -57,23 +95,7 @@ CREATE TABLE IF NOT EXISTS `Seller` (
   `PASSWORDCONSIGN` char(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `User`
---
-
-CREATE TABLE IF NOT EXISTS `User` (
-  `UID` int(10) NOT NULL AUTO_INCREMENT,
-  `USERNAME` char(20) CHARACTER SET utf8 NOT NULL,
-  `PASSWORD` char(32) CHARACTER SET utf8 NOT NULL,
-  `EMAIL` char(30) CHARACTER SET utf8 NOT NULL,
-  `TYPE` tinyint(1) NOT NULL,
-  `BALANCE` int(11) DEFAULT '0',
-  `PHONE` char(11) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`UID`),
-  UNIQUE KEY `ID` (`UID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `User`
@@ -114,21 +136,11 @@ ALTER TABLE `Receiveaddress`
 -- Constraints for table `Usercard`
 --
 ALTER TABLE `Usercard`
-  ADD CONSTRAINT `usercard_ibfk_1` FOREIGN KEY (`USERID`) REFERENCES `USER` (`UID`);
+  ADD CONSTRAINT `usercard_ibfk_1` FOREIGN KEY (`USERID`) REFERENCES `User` (`UID`);
 
 /* group 2 */
 
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders(
-	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	buyer char(50) NOT NULL,
-	seller char(50) NOT NULL,
-	totalprice numeric(15,2),
-	isdelete bit(1) NOT NULL,
-	state char(20) NOT NULL,
-	foreign key (buyer) references buyer(USERNAME) on delete cascade,
-	foreign key (seller) references seller(USERNAME) on delete cascade
-);
+
 DROP TABLE IF EXISTS order_goods;
 CREATE TABLE order_goods(
 	oid INTEGER NOT NULL,
@@ -141,7 +153,7 @@ CREATE TABLE order_goods(
 	foreign key (gid) references goods(id) on delete cascade
 );
 DROP TABLE IF EXISTS order_operation;
-CRAETE TABLE order_operation(
+CREATE TABLE order_operation(
 	oid INTEGER NOT NULL,
 	operation char(20) NOT NULL,
 	time date NOT NULL,
@@ -149,21 +161,9 @@ CRAETE TABLE order_operation(
 	primary key(oid,time),
 	foreign key (oid) references orders(id) on delete cascade
 );
-/* if you are using sqlite please use following instead */
-/* 
-CREATE TABLE transactions(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
-);
- */
 
 /* group 3 */
-DROP TABLE IF EXISTS goods;
-CREATE TABLE goods(
-	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	/* if you are using sqlite please use following instead */
-	/* id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT */
-	type INTEGER NOT NULL
-);
+
 
 DROP TABLE IF EXISTS general_goods;
 CREATE TABLE general_goods(
@@ -179,7 +179,7 @@ CREATE TABLE general_goods(
 	stock INTEGER,
 	description VARCHAR(1024),
 	foreign key (id) references goods(id) on delete cascade,
-	foreign key (seller_id) references user(id) on delete cascade
+	foreign key (seller_id) references User(UID) on delete cascade
 );
 
 DROP TABLE IF EXISTS hotel_room;
@@ -198,7 +198,7 @@ CREATE TABLE hotel_room(
 	date_time BIGINT,
 	suit_type VARCHAR(32),
 	foreign key (id) references goods(id) on delete cascade,
-	foreign key (seller_id) references user(id) on delete cascade
+	foreign key (seller_id) references User(UID) on delete cascade
 );
 
 DROP TABLE IF EXISTS airplane_ticket;
@@ -220,7 +220,7 @@ CREATE TABLE airplane_ticket(
 	non_stop BOOLEAN,
 	carbin_type VARCHAR(32),
 	foreign key (id) references goods(id) on delete cascade,
-	foreign key (seller_id) references user(id) on delete cascade
+	foreign key (seller_id) references User(UID) on delete cascade
 );
 
 DROP TABLE IF EXISTS browse_history;
@@ -231,8 +231,8 @@ CREATE TABLE browse_history(
 	good_id INTEGER,
 	user_id INTEGER,
 	date_time BIGINT,
-	foreign key (good_id) references goods(id) on delete cascade,
-	foreign key (user_id) references user(id) on delete cascade
+	foreign key (good_id) references User(UID) on delete cascade,
+	foreign key (user_id) references User(UID) on delete cascade
 );
 
 DROP TABLE IF EXISTS search_history;
@@ -243,7 +243,7 @@ CREATE TABLE search_history(
 	search_key VARCHAR(256),
 	user_id INTEGER,
 	date_time BIGINT,
-	foreign key (user_id) references user(id) on delete cascade
+	foreign key (user_id) references User(UID) on delete cascade
 );
 
 DROP TABLE IF EXISTS feedback;
@@ -256,8 +256,8 @@ CREATE TABLE feedback(
 	score INTEGER,
 	comment VARCHAR(1024),
 	date_time BIGINT,
-	foreign key (user_id) references user(id) on delete cascade,	
-	foreign key (transaction_id) references transaction(id) on delete cascade
+	foreign key (user_id) references User(UID) on delete cascade,	
+	foreign key (transaction_id) references orders(id) on delete cascade
 	/* if you are using sqlite please use following instead */
 	/* foreign key (transaction_id) references transactions(id) on delete cascade */
 );
@@ -271,12 +271,7 @@ CREATE TABLE shopping_cart(
 	good_id INTEGER,
 	good_count INTEGER,
 	foreign key (good_id) references goods(id) on delete cascade,
-	foreign key (user_id) references user(id) on delete cascade
-);
-
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders(
-	id INTEGER NOT NULL PRIMARY KEY
+	foreign key (user_id) references User(UID) on delete cascade
 );
 
 /* group 4 */
