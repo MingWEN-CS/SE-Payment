18c613c385fb77fa0566027a0da4facae79111d4
DinglongDuan <dinglong.duan@gmail.com>
2013-06-16 21:54:14 +0800

    admin model

--- Lib/Model/AdminModel.class.php
+++ Lib/Model/AdminModel.class.php
@@ -1,8 +1,40 @@
 <?php
 class AdminModel extends Model{
-	public function addAdmin(){
+	protected $_validate = array(
+		array('name','require','admin name is necessary',1),
+		array('name','','the admin name has been registered',1,'unique',1),
+		array('password','require','Password is necessary',1),
+	);
+
+	public function addAdmin($name, $password, $info){
+		$this->id = id;
+		$this->name = name;
+		$this->password = password;
+		$this->info = info;
 		return $this->add();
 	}	
+
+	public function selectAdmin(){
+		return $this->select();
+	}
+
+	public function deleteAdmin($name){
+		return $this->where('name = "'.$name.'"')->delete();
+	}
+
+	public function verifyPassword($name, $password) {
+		$admin = $this->where('name = "'.$name.'"')->find();
+		if (!$admin){
+			return -1;
+		}
+		else {
+			if ($password != $user['password']){
+				return 0;
+			}
+			else return 1;
+		}
+	}
+
 }
 ?>
 
