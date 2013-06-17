9823149ed182674f25e5ac628d936a08b3e5517d
DinglongDuan <dinglong.duan@gmail.com>
2013-06-17 08:02:01 +0800

    login complete

--- Lib/Action/AdminAction.class.php
+++ Lib/Action/AdminAction.class.php
@@ -2,22 +2,30 @@
 // 本类由系统自动生成，仅供测试用途
 class AdminAction extends Action {
     
-    public function index (){
-        $this->display("login");
-    }    
-
-    public function login () {
-    	$se_admin = D('Admin');
-        $name = $this->_post('name');
-        $password = $this->_post('password');
-        $condition['name'] = $name;
-        $admin = $se_admin->where($condition)->find();
-    	if($password == $admin['password']) {
-    		$this->display('header');
-    	}
+    Public function login() {
+        if(IS_AJAX) {
+            $adminname = $this->_post('adminname');
+            $condition['name'] = $adminname;
+            $password = $this->_post('password');
+            $se_admin = D("Admin");
+            $admin = $se_admin->where($condition)->find();
+            if (!$admin){
+                $this->ajaxReturn('', 'Admin Does Not Exsit', 0);
+            }
+            else if ($password != $admin[password]){
+                $this->ajaxReturn('', 'Wrong Password', 0);
+            }
+            else {
+                $this->ajaxReturn('', 'Login Successfully', 1);
+            }
+        }
         else {
             $this->display();
         }
+    }
+
+    Public function admin() {
+        $this->display();
     }
 }
 ?>
