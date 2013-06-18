5987ad4f59334650718c24d04900bd99526c68ab
WTYJack <wtyjack@gmail.com>
2013-06-18 19:47:40 +0800

    add feedback model

---
+++ Lib/Model/FeedbackModel.class.php
@@ -0,0 +1,29 @@
+<?php
+class FeedbackModel extends Model{
+	public function score_update($goods_id){
+		$score_set = $this->where('goods_id='.$goods_id)->select();
+		$score_sum = 0;
+		foreach($score_set as $score_each) {
+			$score_sum += $score_each['score'];
+		}
+		$score_avg = $score_sum / count($score_set);
+		$Goods = D('Goods');
+		
+		$goodsResult = $Goods->where('id='.$goods_id)->find();
+		if($goodsResult['type'] == 1) {
+			$GoodsAllKinds = D('GeneralGoods');
+		}
+		else if($goodsResult['type'] == 2) {
+			$GoodsAllKinds =  D('HotelRoom');
+		}
+		else if($goodsResult['type'] == 3) {
+			$GoodsAllKinds = D('AirplaneTicket');
+		}
+		$data['score'] = $score_avg;
+		return $GoodsAllKinds->where('id='.$goods_id)->save($data);
+	}
+	protected $_auto = array(
+		array('id'),
+	);
+}
+?>
