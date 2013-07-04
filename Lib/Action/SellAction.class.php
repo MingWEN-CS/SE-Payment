163d1e7ca2f28482ba048ea353164d9f2724633b
lcy <lcy930822@163.com>
2013-07-04 10:54:48 +0800

    add some comment

--- Lib/Action/SellAction.class.php
+++ Lib/Action/SellAction.class.php
@@ -3,30 +3,38 @@ import("@.Org.Net.UploadFile");
 
 class SellAction extends Action {
 	
+	// index page, to show all goods this seller sell
     public function index($info = null, $good_type = 'general-goods'){
+		// if there is info to output
 		if (!$info)
 			$info = $_POST['info'];
+		// get good_type, switch to corresponding tab
 		if (!$good_type)
 			$good_type = $_POST['good_type'];
 		$user_id = $_SESSION['uid'];
 		$seller = D('Seller');
+		// check if a valid seller
 		if (!$user_id || !$seller->where('UID = '.$user_id)->find())
 			$this->error('To sell goods, you must login as a seller!');
+		// output all goods the seller sell
 		$general = D('GeneralGoods');
 		$hotel = D('HotelRoom');
 		$airplane = D('AirplaneTicket');
 		$filter = 'seller_id = '.$user_id;
 		$this->assign('good_type', $good_type);
+		// find all general_goods
 		$goods = $general->where($filter)->select();
 		for($i = 0;$i < count($goods);$i++){
 			$goods[$i]['score'] = round($goods[$i]['score'], 2);
 		}
 		$this->assign('general_goods', $goods);
+		// find all hotel
 		$goods = $hotel->where($filter)->select();
 		for($i = 0;$i < count($goods);$i++){
 			$goods[$i]['score'] = round($goods[$i]['score'], 2);
 		}
 		$this->assign('hotel_room', $goods);
+		// find all airplane
 		$goods = $airplane->where($filter)->select();
 		for($i = 0;$i < count($goods);$i++){
 			$goods[$i]['score'] = round($goods[$i]['score'], 2);
@@ -37,9 +45,11 @@ class SellAction extends Action {
 		$this->display('index');
     }
 	
+	// add a good or modify an existed one
 	public function add(){
 		$good_type = $this->_param('good_type');
 		$good_id = $this->_param('good_id');
+		// open correct database by given good type
 		switch ($good_type) {
 		case 'general-goods':
 			$goods = D('GeneralGoods');
@@ -57,17 +67,23 @@ class SellAction extends Action {
 			$this->error("Unknown good's type!");
 			break;
 		}
+		// POST means sumbit the form
+		// i.e. add or update good info
 		if (IS_POST) {
+			// if add a new good
 			if ($isCreate = !$good_id) {
 				$data = array(
 					'type' => $type,
 				);
+				// add into good table and get it's id
 				$good_id = D('Goods')->add($data);
 			}
+			// if successly created(or prepared to update) a record by POSTed info
 			if ($goods->create()) {
+				// filling filed which are not been POSTed
 				$goods->id = $good_id;
 				$goods->seller_id = $_SESSION['uid'];
-				// change time to timestamp
+				// change time to timestamp, string formating
 				switch ($good_type) {
 				case 'general-goods':
 					break;
@@ -79,31 +95,44 @@ class SellAction extends Action {
 					$goods->arrival_date_time = strtotime($goods->arrival_date_time);
 					break;
 				}
+				// dealing with uploaded picture
 				if (!empty($_FILES)) {
 					$upload = new UploadFile();
+					// setting maximum upload file size
 					$upload->maxSize = 1048576;
+					// setting allowed format
 					$upload->allowExts = array('bmp', 'jpg', 'gif', 'png', 'jpeg');
+					// setting upload path
 					$upload->savePath = './Upload/';
 					$upload->uploadReplace = true;
+					// if upload failed
 					if (!$upload->upload()) {
 						$this->error($upload->getErrorMsg());
+					// upload success
 					} else {
 						$info = $upload->getUploadFileInfo();
+						// save picture name to uri field
 						$goods->image_uri = $info[0]['savename'];
 					}
 				}
+				// add or update records
 				if ($isCreate) {
 					$goods->add();
 				} else {
 					$goods->save($data);
 				}
 				$this->success('Operation success!');
+			// create record failed
 			} else {
 				$this->error($goods->getError());
 			}
+		// GET means auto fill good info
+		// i.e. using modify function
 		} else {
+			// if it's a modify rather than add a new one
 			if ($good_id) {
 				$good = $goods->where('id = '.$good_id)->find();
+				// do time formating, from timestamp to string
 				switch ($good_type) {
 				case 'general-goods':
 					break;
@@ -118,8 +147,10 @@ class SellAction extends Action {
 					$good['arrival_date_time'] = date('Y-m-d H:i:s', $good['arrival_date_time']);
 					break;
 				}
+				// fill goods info to form
 				$this->assign('good', $good);
 			}
+			// set good type
 			$this->assign('good_type', $good_type);
 			
 			// 3 kinds goods' source place options
@@ -128,7 +159,6 @@ class SellAction extends Action {
 			$this->assign('airplane_ticket_arrival_place', AirplaneTicketModel::getArrivalPlaceObjectsArray());
 			// hotel suit options
 			$this->assign('hotel_room_suit', HotelRoomModel::getHotelRoomSuitArray());
-			$this->assign('hotel_stars', array(1,2,3,4,5));
 			// airplane carbin options
 			$this->assign('airpalne_ticket_carbin', AirplaneTicketModel::getAirplaneTicketCarbinArray());
 			
@@ -136,10 +166,7 @@ class SellAction extends Action {
 		}
 	}
 	
-	public function upload() {
-		$this->error('ge');
-	}
-	
+	// delete some goods, take an array as parameter
 	public function delete(){
 		$goods = D('Goods');
 		$ret = true;
@@ -147,9 +174,7 @@ class SellAction extends Action {
 		foreach ($_POST['good_ids'] as $good_id) {
 			$ret &= $goods->where('id = '.$good_id)->delete();
 		}
-
-		$this->redirect('Sell/index');
-		// $this->index($ret ? 'Deletion succeeded!' : 'Deletion failed!', $_POST['good_type']);
+		$this->index($ret ? 'Deletion succeeded!' : 'Deletion failed!', $_POST['good_type']);
 	}
 	
 }
--- Lib/Model/ShoppingCartModel.class.php
+++ Lib/Model/ShoppingCartModel.class.php
@@ -3,18 +3,18 @@ class ShoppingCartModel extends Model{
 	
 	public function modifyCount($user_id, $good_id, $good_count, $add) {
 		$good = $this->where('user_id = '.$user_id.' AND good_id = '.$good_id);
-		if ($add == 'true') {
-			if (D("Goods")->getStockById($good_id) > $good_count)
+		if ($add == 'true')
+			// check whether stock is enough
+			if (getStockById($good_id) < $good_count)
 				return $good->setInc('good_count');
 			else
 				return false;
-		}
-		else {
+		else
+			// count cannot be 0
 			if ($good_count > 1)
 				return $good->setDec('good_count');
 			else
 				return false;
-		}
 	}
 }
 ?>
